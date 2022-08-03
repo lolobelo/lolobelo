96e980529f9c75fd1721b2d106f7364cd6aad85d
loloplox <rodrigomaldonadov8@gmail.com>
2022-08-03 00:41:45 -0500

    deploying firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,21 +1,39 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
+
 import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
+
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
 // Your web app's Firebase configuration
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDqPahdZbnIDGQivq6Nc-qNOysRB-KT2pk",
-    authDomain: "lolobelo-page.firebaseapp.com",
-    projectId: "lolobelo-page",
-    storageBucket: "lolobelo-page.appspot.com",
-    messagingSenderId: "125280205865",
-    appId: "1:125280205865:web:e0f4037f952efea93c7038",
-    measurementId: "G-G5XMSRHY7S"
+
+    apiKey: "AIzaSyC0bD2mZBfnoQ4QoJvUa5r9S93yphChBv0",
+
+    authDomain: "lolobelo-1cf00.firebaseapp.com",
+
+    projectId: "lolobelo-1cf00",
+
+    storageBucket: "lolobelo-1cf00.appspot.com",
+
+    messagingSenderId: "441440385308",
+
+    appId: "1:441440385308:web:d7082816d28c25102f22d9",
+
+    measurementId: "G-MR5KX09XWL"
+
 };
 
+
 // Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
+
 const analytics = getAnalytics(app);
